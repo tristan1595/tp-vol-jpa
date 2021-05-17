fa11c1b9d2f34e398d058d2d99ff0f2fa72003a7
Lea Dumont <leadumont2512@gmail.com>
2021-05-17 12:38:01 +0200

    DAO vol billet passager

--- src/main/java/sopra/vol/repository/jpa/BilletRepositoryJpa.java
+++ src/main/java/sopra/vol/repository/jpa/BilletRepositoryJpa.java
@@ -1,34 +1,134 @@
 package sopra.vol.repository.jpa;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.vol.Application;
 import sopra.vol.model.Billet;
+import sopra.vol.model.Vol;
 import sopra.vol.repository.IBilletRepository;
 
 public class BilletRepositoryJpa implements IBilletRepository {
 
 	@Override
 	public List<Billet> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Billet> billets = new ArrayList<Billet>();
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Billet> query = em.createQuery("select b from Billet b", Billet.class);
+
+			billets = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return billets;
 	}
 
 	@Override
 	public Billet findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Billet billet = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			billet = em.find(Billet.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return billet;
 	}
 
 	@Override
 	public Billet save(Billet obj) {
-		// TODO Auto-generated method stub
-		return null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			obj = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return obj;
 	}
 
 	@Override
 	public void delete(Billet obj) {
-		// TODO Auto-generated method stub
-		
-	}
+		EntityManager em = null;
+		EntityTransaction tx = null;
 
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		
+		}
+
+	}
 }
