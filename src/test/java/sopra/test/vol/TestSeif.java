1c74c57642490e65d1428f105530df826452f6e3
Seif <seif@>
2021-05-17 10:41:49 +0200

    Entity client adresse particulier entreprise

--- src/test/java/sopra/test/vol/TestSeif.java
+++ src/test/java/sopra/test/vol/TestSeif.java
@@ -1,9 +1,14 @@
 package sopra.test.vol;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class TestSeif {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("tp-vol-jpa");
+		
+		emf.close();
 
 	}
 
